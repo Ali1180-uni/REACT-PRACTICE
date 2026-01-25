b03f686798a2fdd89d3acb1e9f309d69aa07e33d
Ali1180-uni <thegreat1552005@gmail.com>
2026-01-25 18:53:19 +0500

    Todo List Completed

--- TodoList/src/App.jsx
+++ TodoList/src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from "react";
 import Navbar from "./Components/Navbar";
 import { nanoid } from "nanoid";
 import ShowTodos from "./Components/ShowTodos";
+import Completed from "./Components/Completed";
 
 import "./App.css";
 
 function App() {
   const [todo, setTodo] = useState([]);
   const [newTodo, setnewTodo] = useState("");
-  const [Completed, setCompleted] = useState(false)
+  const [Completed1, setCompleted] = useState(false);
 
   const handle = (e) => {
     setnewTodo(e.target.value);
@@ -17,6 +18,10 @@ function App() {
   const AddTodo = () => {
     setTodo([...todo, { id: nanoid(), text: newTodo, isCompleted: false }]);
     setnewTodo("");
+  };
+
+  const turn = () => {
+    setCompleted(() => !Completed1);
   };
 
   return (
@@ -39,8 +44,17 @@ function App() {
             ADD
           </button>
         </div>
-        <button onClick={Switch}>Check the Completed Tasks</button>
-        <ShowTodos todo={todo} setTodo={setTodo} setnewTodo={setnewTodo} />
+        <button
+          className="bg-amber-500/90 hover:bg-amber-500 text-white font-medium py-2 px-6 rounded-full border border-amber-400/50 shadow-lg backdrop-blur-sm focus:ring-4 focus:ring-amber-300 my-2"
+          onClick={turn}
+        >
+          Check the Completed Tasks
+        </button>
+        {Completed1 ? (
+          <Completed todo={todo} />
+        ) : (
+          <ShowTodos todo={todo} setTodo={setTodo} setnewTodo={setnewTodo} />
+        )}
       </div>
     </>
   );
--- TodoList/src/Components/Completed.jsx
+++ TodoList/src/Components/Completed.jsx
@@ -1,25 +1,20 @@
-import React from 'react'
-
-const Completed = ({todo}) => {
- return <div>
-      {todo.map((items) => {
-        return (
-          <div
-            className="ShowTodos flex gap-4 items-center justify-center p-3 m-2 bg-white rounded-lg shadow-md max-w-lg mx-auto"
-            key={items.id}
-          >
-            <p
-              className="flex-1 text-lg"
-              style={{
-                textDecoration: items.isCompleted ? "line-through" : "none",
-              }}
+const Completed = ({ todo }) => {
+  return (
+    <div>
+      {todo
+        .filter((item) => item.isCompleted)
+        .map((items) => {
+          return (
+            <div
+              className="ShowTodos flex gap-4 items-center justify-center p-3 m-2 bg-white rounded-lg shadow-md max-w-lg mx-auto"
+              key={items.id}
             >
-              {!(items.isCompleted) && items.text}
-            </p>
-          </div>
-        );
-      })}
+              <p className="flex-1 text-lg line-through">{items.text}</p>
+            </div>
+          );
+        })}
     </div>
-}
+  );
+};
 
-export default Completed
+export default Completed;
