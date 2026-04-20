1cff9b70f5b9517aae041c38948bec281d9f2655
Ali1180-uni <thegreat1552005@gmail.com>
2026-04-20 19:19:19 +0500

    Figlet Added

--- Basics/src/App.jsx
+++ Basics/src/App.jsx
@@ -1,22 +1,50 @@
-import { useState, useCallback } from "react";
-import Navbar from "./Components/Navbar"
-import "./App.css";
+// import { useState, useCallback } from "react";
+// import Navbar from "./Components/Navbar"
+// import "./App.css";
+import { useState } from "react";
+import figlet from "figlet";
+import standardFont from "figlet/importable-fonts/Standard.js";
+
+figlet.parseFont("Standard", standardFont);
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [adjective, setAdjective] = useState("good");
+const [count, setCount] = useState("");
+function checkFiglet(){
+  figlet.text(count, { font: "Standard" }, function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(data);
+  setCount(data);
+});
+}
+function changedata(e){
+  setCount(e.target.value)
+}
+  // const [count, setCount] = useState(0);
+  // const [adjective, setAdjective] = useState("good");
 
   // const getAdjective = () => {
   //   return "another" + count
   // }
 
-  const getAdjective = useCallback(() => {
-    return "another" + count;
-  }, [count]);
+  // const getAdjective = useCallback(() => {
+  //   return "another" + count;
+  // }, [count]);
 
   return (
     <>
-      <h1>Vite + React</h1>
+    <input type="text" onChange={changedata}  />
+    <h1 className="name" style={{ whiteSpace: "pre", fontFamily: "monospace" }}>{count}</h1>
+    <button onClick={checkFiglet}>Submit</button>
+
+
+
+
+
+      {/* <h1>Vite + React</h1>
       <Navbar adjective={"good"} getAdjective={getAdjective} />
       <div className="card">
         <button
@@ -26,7 +54,7 @@ function App() {
         >
           count is {count}
         </button>
-      </div>
+      </div> */}
     </>
   );
 }
