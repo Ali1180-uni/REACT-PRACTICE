2ab522fb52aa11c6ce7aed3852453967f5166f27
Ali1180-uni <thegreat1552005@gmail.com>
2026-07-11 18:29:02 +0500

    Notes Added

--- TS/Backend With TypeScript/src/index.ts
+++ TS/Backend With TypeScript/src/index.ts
@@ -1,11 +1,25 @@
-import express from "express";
-import type { Express } from "express"; // Very imp in Express
-const app: Express = express();
+import express from 'express'
+import type {Express} from 'express'
 
-app.get("/", (req, res) => {
-  res.send("Hi Ali");
-});
-const port: number = 8000;
-app.listen(port, (): void => {
-  console.log(`its working on ${port}`);
-});
+const PORT = 8000
+const app:Express = express()
+
+/*
+CHALLENGE: Respond with some data!
+1. Create an object called `pet` before the `/` route that includes:
+   - name (string)
+   - species (string)
+   - adopted (boolean)
+   - age (number)
+2. Type your new pet object (inline or using a custom type)
+3. Update your existing GET `/` route to return that pet object
+4. Compile the TypeScript and run the resulting JavaScript to see it in action
+*/
+
+app.get('/', (req, res)=> {
+  res.json({})
+})
+
+app.listen(PORT, ():void =>{
+  console.log("Listening on port: ", PORT)
+})
