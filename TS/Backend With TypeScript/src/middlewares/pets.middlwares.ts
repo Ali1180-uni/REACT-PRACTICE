75b74e1053037333e81d73b5f8d39ac6051994a3
Ali1180-uni <thegreat1552005@gmail.com>
2026-07-12 22:54:35 +0500

    Setup Pet Middlewares

---
+++ TS/Backend With TypeScript/src/middlewares/pets.middlwares.ts
@@ -0,0 +1,45 @@
+import type {Request, Response, NextFunction} from 'express'
+
+/*
+CHALLENGE: Get specific with the Request and Response generics
+1. Think about what params we might be getting in the request
+2. Consider what we might be responding with
+*/
+
+export const validateId = (
+  req:Request<{id:string}>, 
+  res:Response<{message:string}>, 
+  next:NextFunction
+) => {
+  const {id} = req.params
+  if (!/^\d+$/.test(id)) { // This is Regex which is used to identify specific things like numbers, emails & passwords etc
+    res.status(400).json({message: "Pet ID must be a number"})
+  } else {
+    next()
+  }
+}
+
+/*
+CHALLENGE: Create a `pleaseAuth` middleware
+1. Write a standalone function called `pleaseAuth`
+2. It should take req, res, and next
+3. If query.password === "please", call next()
+4. Otherwise, send a 401 with a message in JSON
+5. Type req, res, and next
+6. Attach this middleware to the `/:id` route
+   
+TESTS:
+/pets/1 - should result in 401 Unauthorised error
+/pets/1?password=please - we should get Bella the dog
+*/
+
+export const pleaseAuth = (req:Request<{}, unknown, {}, {password?:string}>, res: Response<{message: string}>, next: NextFunction): void => {
+    const {password} = req.query
+    if(password !== "please"){
+        res.status(401).json({message: "Auth Fails"})
+        return
+    }else{
+        next()
+    }
+
+}
--- TS/Backend With TypeScript/src/routes/pets.routes.ts
+++ TS/Backend With TypeScript/src/routes/pets.routes.ts
@@ -1,9 +1,10 @@
 import Express from "express";
 import type { Router } from "express";
 import { getPetById, getPets } from "../controllers/pets.controller.js";
+import { validateId, pleaseAuth } from "../middlewares/pets.middlwares.js";
 
 export const petRouter: Router = Express.Router();
 
 petRouter.get("/", getPets);
 
-petRouter.get("/:id", getPetById);
+petRouter.get("/:id", pleaseAuth, validateId, getPetById);
