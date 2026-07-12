c4999cafbd758fa400cb67b4d01871b377a5a645
Ali1180-uni <thegreat1552005@gmail.com>
2026-07-12 21:12:02 +0500

    Setup Pet Router

--- TS/Backend With TypeScript/src/index.ts
+++ TS/Backend With TypeScript/src/index.ts
@@ -1,64 +1,22 @@
 import express from "express";
 import cors from "cors";
 import type { Express, Request, Response } from "express";
-import { pets, Pet } from "./data/pets.js";
+
+import { petRouter } from "./routes/pets.routes.js";
 
 const PORT = 8000;
 const app: Express = express();
 app.use(cors());
 
-type typeQueryPrams = {
-  species?: string;
-  adopted?: string;
-}
-
 // app.get("/", (req: Request, res: Response<Pet[]>): void => {
 //   res.json(pets);
 // });
 
-
-app.get(
-  "/:id",
-  (
-    req: Request<{ id: string }>,
-    res: Response<Pet | { message: string }>,
-  ): void => {
-    const { id } = req.params;
-    const pet: Pet | undefined = pets.find(
-      (pet: Pet): boolean => pet.id.toString() === id,
-    );
-    if (!pet) {
-      res.status(404).json({ message: "not found" });
-      return
-    }
-    res.json(pet);
-  },
-);
+app.get("/:id", petRouter);
 
 // http://localhost:8000/?adopted=true check by this
 // More Flexible
-app.get('/', (
-  req:Request<{}, unknown, {}, typeQueryPrams>, 
-  res:Response<Pet[]>
-):void=> {
-  const {species, adopted} = req.query
-
-  let filteredPets:Pet[] = pets
-  
-  if (species){
-    filteredPets = filteredPets.filter((pet:Pet):boolean=>
-      pet.species.toLowerCase() === species.toLowerCase()
-    )
-  }
-
-  if (adopted){
-    filteredPets = filteredPets.filter((pet:Pet):boolean=>
-      pet.adopted === JSON.parse(adopted) // becz we got the true value not in boolean
-    )
-  }
-
-  res.json(filteredPets)
-})
+app.get("/", petRouter);
 
 // Species Specified Setup only
 // app.get(
@@ -80,7 +38,6 @@ app.get('/', (
 //     res.json(filteredPets);
 //   },
 // );
-
 
 app.use((req: Request, res: Response<{ message: string }>): void => {
   // Here Response is Generic and message decides the type of JSON type which returns
